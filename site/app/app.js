3f9f8cae218b7478c253f240050b43f8c2b44fd6
f <f>
2021-05-25 21:48:55 -0300

    #CR-79 inicialice app

---
+++ site/app/app.js
@@ -0,0 +1,31 @@
+// components
+import Nav from './components/Nav.js'
+
+const Home     = { template: '<div>Home</div>' }
+const NotFound = { template: '<div>Not found</div>' }
+
+//router
+const routes = [
+  { path: '/', component: Home },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  // { path: '*', component: NotFound }
+]
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = VueRouter.createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: VueRouter.createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = Vue.createApp({})
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+// register components
+app.component('app-nav', Nav)
+
+app.mount('#app')
---
+++ site/app/components/Nav.js
@@ -0,0 +1,20 @@
+export default { 
+  template : `
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top" id="app-nav">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./images/logo.png" alt="log" width="46" >
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mt-2">
+        <li class="nav-item">
+          <!-- <router-link to="/" class="nav-link"></router-link> -->
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>`
+}
