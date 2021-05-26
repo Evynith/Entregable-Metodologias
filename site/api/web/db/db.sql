89b9c356c05d0f985808529013b08d5a66f7e5dd
Nico Contreras <nicocontreras538@gmail.com>
2021-05-25 21:07:05 -0300

    #CR-55 inicialice la api

---
+++ site/api/web/db/db.sql
@@ -0,0 +1,1 @@
+/** Script de creación de la base de datos */
---
+++ site/api/web/mvc/controllers/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class ApiController {
+
+    protected $view;
+    private $data; 
+
+    public function __construct() {
+        $this->view = new JSONView();
+        $this->data = file_get_contents("php://input");
+    }
+
+    protected function getData(){ 
+        return json_decode($this->data); 
+    }  
+
+}
+
+?>
