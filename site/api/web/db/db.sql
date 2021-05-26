6308df7fc75db86c2f69ec32a21a46a186614450
Francisco Vaninetti <francisco.vaninetti@asivas.com.ar>
2021-05-26 20:20:47 -0300

    CR-60 creacion de la tabla materiales (SQL)

--- site/api/web/db/db.sql
+++ site/api/web/db/db.sql
@@ -1,1 +1,8 @@
 /** Script de creación de la base de datos */
+CREATE TABLE unc_249456.materiales (
+    id SERIAL NOT NULL,    
+    nombre varchar(20)   NOT NULL,
+    imagen bytea  NOT NULL,
+    descripcion varchar(200),
+    CONSTRAINT materiales_pk PRIMARY KEY (id)
+);
