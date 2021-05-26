f894118a5f1164439d971ae51bc18e08f57825ea
Nico Contreras <nicocontreras538@gmail.com>
2021-05-25 23:02:03 -0300

    #CR-71 creacion tabla franja_horaria

--- site/api/web/db/db.sql
+++ site/api/web/db/db.sql
@@ -1,1 +1,13 @@
 /** Script de creación de la base de datos */
+DROP TABLE IF EXISTS unc_249456.franja_horaria;
+CREATE TABLE unc_249456.franja_horaria (
+    id SERIAL NOT NULL,
+    nombre varchar(20)  NOT NULL,
+    CONSTRAINT franja_horaria_pk PRIMARY KEY (id)
+);
+--INSERTS
+INSERT INTO unc_249456.franja_horaria (nombre)
+VALUES ('9 a 12hs');
+
+INSERT INTO unc_249456.franja_horaria (nombre)
+VALUES ('13 a 17hs');
