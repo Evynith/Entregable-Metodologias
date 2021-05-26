a7713b5ae6f5df30a614a1345b21b17732505085
f <f>
2021-05-25 21:18:55 -0300

    #CR-56 agregue conexion a la base de datos

--- site/api/web/mvc/models/Model.php
+++ site/api/web/mvc/models/Model.php
@@ -3,6 +3,17 @@
 class Model {
     protected $db;
     function __construct() {
-   
+        $HOST = 'dbases.exa.unicen.edu.ar';
+        $PORT = '6432';
+        $DB_NAME = 'cursada2021';
+        $DB_USER = 'unc_249456';
+        $DB_PASS = '249456';
+        try {
+            $this->db = new PDO("pgsql:host=${HOST};port=${PORT};dbname=${DB_NAME};user=${DB_USER};password=${DB_PASS}");
+            var_dump("estoy conectado a la db");
+        }
+        catch (PDOException $e) {
+            var_dump($e);
+        }
     }
 }
