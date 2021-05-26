98f891d0e66c44befeb00dedaf126aa2eed07b6a
Muguckz <fedemuguruza@gmail.com>
2021-05-26 00:31:45 -0300

    #CR-73 Se sube la creación de la tabla volumen de materiales

--- site/api/web/db/db.sql
+++ site/api/web/db/db.sql
@@ -6,8 +6,17 @@ CREATE TABLE unc_249456.franja_horaria (
     CONSTRAINT franja_horaria_pk PRIMARY KEY (id)
 );
 --INSERTS
-INSERT INTO unc_249456.franja_horaria (nombre)
-VALUES ('9 a 12hs');
+INSERT INTO unc_249456.franja_horaria (nombre) VALUES ('9 a 12hs');
+INSERT INTO unc_249456.franja_horaria (nombre) VALUES ('13 a 17hs');
 
-INSERT INTO unc_249456.franja_horaria (nombre)
-VALUES ('13 a 17hs');
+DROP TABLE IF EXISTS unc_249456.volumen_materiales; 
+CREATE TABLE unc_249456.volumen_materiales (
+    id SERIAL NOT NULL,
+    categoria varchar(20) NOT NULL,
+    CONSTRAINT volumen_materiales_pk PRIMARY KEY (id)
+);
+
+INSERT INTO unc_249456.volumen_materiales (categoria) VALUES ('Caja');
+INSERT INTO unc_249456.volumen_materiales (categoria) VALUES ('Baúl de un auto');
+INSERT INTO unc_249456.volumen_materiales (categoria) VALUES ('Camioneta');
+INSERT INTO unc_249456.volumen_materiales (categoria) VALUES ('Camión');
