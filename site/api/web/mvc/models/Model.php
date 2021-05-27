b7f102f45cb2bec98b52f52bbfa1d666a8d66a17
franf16 <franchinif16@gmail.com>
2021-05-27 11:50:15 -0300

    #CR-83 pase conexion a la db a una clase estatica

--- site/api/web/mvc/models/Model.php
+++ site/api/web/mvc/models/Model.php
@@ -3,23 +3,41 @@
 class Model {
     protected $db;
     function __construct() {
-        $HOST = 'dbases.exa.unicen.edu.ar';
-        $PORT = '6432';
-        $DB_NAME = 'cursada2021';
-        $DB_USER = 'unc_249456';
-        $DB_PASS = '249456';
-        try {
-            $this->db = new PDO(
-                "pgsql:host=${HOST};port=${PORT};dbname=${DB_NAME}",
-                $DB_USER,
-                $DB_PASS,
-                array(
-                    PDO::ATTR_TIMEOUT => 1,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                ));
+        $this->db = Database::getConnection();
+    }
+}
+
+class Database {
+
+    private static $db; 
+
+    private function __construct() {}
+
+    public static function getConnection() {
+        if (isset(self::$db)) {
+            return self::$db;
         }
-        catch (PDOException $e) {
-            // var_dump($e);
+        else {
+            $HOST = 'dbases.exa.unicen.edu.ar';
+            $PORT = '6432';
+            $DB_NAME = 'cursada2021';
+            $DB_USER = 'unc_249456';
+            $DB_PASS = '249456';
+            try {
+                self::$db = new PDO(
+                    "pgsql:host=${HOST};port=${PORT};dbname=${DB_NAME}",
+                    $DB_USER,
+                    $DB_PASS,
+                    array(
+                        PDO::ATTR_TIMEOUT => 1,
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                    ));
+                return self::$db;
+            }
+            catch (PDOException $e) {
+                // var_dump($e);
+                self::$db = false;
+            }
         }
     }
 }
