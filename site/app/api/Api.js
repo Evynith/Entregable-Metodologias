246641c262e8471c62a58a30387efd4946ac3246
franf16 <franchinif16@gmail.com>
2021-06-13 12:53:16 -0300

    Merge app

--- site/app/api/Api.js
+++ site/app/api/Api.js
@@ -3,17 +3,24 @@ export default class Api {
   static async postAvisoRetiro(a) {
     // const r = await Api.fetchLocalAPI('aviso_retiro', {
     let r;
+    let options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(a)
+    }
     try {
-      r = await Api.fetchHerokuAPI('aviso_retiro', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(a)
-      })
+      r = await Api.fetchHerokuAPI('aviso_retiro', options)
     }
     catch (e) {
-      r = {
-        ok: false,
-        mensaje: 'Error de conexión'
+      console.log(e, '#Posteando datos a API local...')
+      try {
+        r = await Api.fetchLocalAPI('aviso_retiro', options)
+      }
+      catch (e2) {
+        r = {
+          ok: false,
+          mensaje: 'Error de conexión'
+        }
       }
     }
     // console.log(r)
@@ -37,16 +44,21 @@ export default class Api {
       json = await Api.fetchHerokuAPI(endpoint)
     }
     catch (e) {
-      console.log(e)
-      console.log('#Obteniendo datos de json local...')
-      json = await Api.fetchLocalJSON(endpoint)
+      try {
+        console.log(e, '#Obteniendo datos de API local...')
+        json = await Api.fetchLocalAPI(endpoint)
+      }
+      catch (e2) {
+        console.log(e2, '#Obteniendo datos de json local...')
+        json = await Api.fetchLocalJSON(endpoint)
+      }
     }
     return json
   }
 
   static fetchHerokuAPI(endpoint, options = { }) {
     const url = `https://coop-rec-api.herokuapp.com/${endpoint}`
-    return fetch(url, options).then(r => r.json()).catch(e => console.log(e))
+    return fetch(url, options).then(r => r.json())
   }
 
   static fetchLocalAPI(endpoint, options = {}) {
