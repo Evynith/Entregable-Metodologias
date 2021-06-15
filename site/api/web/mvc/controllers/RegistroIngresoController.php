b2d6f23b2330002e5ecf061b7117575c12951baa
Nico Contreras <nicocontreras538@gmail.com>
2021-06-15 18:35:38 -0300

    #CR-148 complemento el RegistroIngresoController

--- site/api/web/mvc/controllers/RegistroIngresoController.php
+++ site/api/web/mvc/controllers/RegistroIngresoController.php
@@ -16,9 +16,31 @@ class RegistroIngresoController extends ApiController {
     }
 
     public function postRegistroIngreso() {
-      
+     
+        $respuesta = []; 
+        $codigo;
+        $data = $this->getData();
+        $id = $this->modelRegistroIngreso.postRegistroIngreso($data);
+
+        if(isset($id)){
+            $respuesta[ 'ok' ] = true;
+            $respuesta[ 'id' ] = $id;
+            $respuesta[ 'mensaje' ] = "El registro de ingreso de materiales fue cargado con exito";      
+            $codigo = 200;
+        }
+
+        foreach ($data->materiales_cargados as $material) {
+
+            if (!$this->modelRegistroIngreso->postMaterialCargado($data)) {
+
+                $respuesta[ 'ok' ] = false;
+                $respuesta[ 'mensaje' ] = "Los materiales no pudieron ser cargados";
+                $codigo = 400;
+            }
+            
+        }
+        $this->view->response($respuesta, $codigo);
         
-       
     }
 
 }  
