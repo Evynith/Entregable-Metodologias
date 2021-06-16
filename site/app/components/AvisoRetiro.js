35370f2b33e299ce774ca190d4c3dd932d819e0f
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2021-06-16 17:27:46 -0300

    #CR-164 responsive del área aviso retiro ateriales

--- site/app/components/AvisoRetiro.js
+++ site/app/components/AvisoRetiro.js
@@ -10,55 +10,63 @@ const AvisoRetiroTemplate = `
 <section v-else>
   <h1 class="fs-1 text-center"> Crear aviso de retiro de materiales</h1>
 
-  <form v-on:submit.prevent="post" class="p-2 form-control-sm" id="form-ar">
-    <div class="form-group mb-2">
-      <label>Nombre *</label>
-      <input v-model.lazy="ar.nombre" type="text" class="form-control form-control-sm">
-    </div>
-    <div class="form-group mb-2">
-      <label>Apellido *</label>
-      <input v-model.lazy="ar.apellido" type="text" class="form-control form-control-sm">
-    </div>
-    <div class="form-group mb-2">
-      <label>Direccion *</label>
-      <input v-model="ar.direccion" type="text" class="form-control form-control-sm">
-    </div>
-    <div class="form-group mb-2">
-      <label>Telefono *</label>
-      <input v-model="ar.telefono" type="text" class="form-control form-control-sm">
-    </div>
-    <div class="form-group mb-2">
-      <label for= "selectHorario">Franja horaria *</label>
-      <select v-model="ar.id_horario" class="form-select form-select-sm" aria-label=".form-control-sm example" id= "selectHorario">
-        <option v-for="f of franjasHorarias"
-          :value="f.id"
-        >{{ f.nombre }}</option>
-      </select>
-    </div>
-    <div class="form-group mb-2">
-      <label for= "selectVolumen">Volumen de los materiales (entran en ...) *</label>
-      <select v-model="ar.id_volumen" class="form-select form-select-sm" aria-label=".form-control-sm example" id= "selectVolumen">
-        <option v-for="m of volumenesMateriales"
-          :value="m.id"
-        >{{ m.categoria }}</option>
-      </select>
-    </div>
-    <div class="form-group mb-3">
-      <label for="formFile">Cargar imagen de los materiales <small>(opcional)</small></label>
-      <input-file v-model="ar.foto" type="img" accept="image/*"></input-file>
-    </div>
-    
-    <div class="d-grid gap-2">
-      <button :disabled="!verificado || posting" class="btn btn-primary btn-block w-100 py-2 text-uppercase">
-        <template v-if="!posting">Enviar</template>
-        <div v-else class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+  <div class="row">
+    <div class="col-md d-none d-md-block"></div>
+
+    <div class="col-md">
+      <form v-on:submit.prevent="post" class="p-2 form-control-sm" id="form-ar">
+        <div class="form-group mb-2">
+          <label>Nombre *</label>
+          <input v-model.lazy="ar.nombre" type="text" class="form-control form-control-sm">
         </div>
-      </button>
-      <button @click.prevent="$router.go(-1)" class="btn btn-link btn-block w-100 py-2 text-uppercase">Cancelar</button>
+        <div class="form-group mb-2">
+          <label>Apellido *</label>
+          <input v-model.lazy="ar.apellido" type="text" class="form-control form-control-sm">
+        </div>
+        <div class="form-group mb-2">
+          <label>Direccion *</label>
+          <input v-model="ar.direccion" type="text" class="form-control form-control-sm">
+        </div>
+        <div class="form-group mb-2">
+          <label>Telefono *</label>
+          <input v-model="ar.telefono" type="text" class="form-control form-control-sm">
+        </div>
+        <div class="form-group mb-2">
+          <label for= "selectHorario">Franja horaria *</label>
+          <select v-model="ar.id_horario" class="form-select form-select-sm" aria-label=".form-control-sm example" id= "selectHorario">
+            <option v-for="f of franjasHorarias"
+              :value="f.id"
+            >{{ f.nombre }}</option>
+          </select>
+        </div>
+        <div class="form-group mb-2">
+          <label for= "selectVolumen">Volumen de los materiales (entran en ...) *</label>
+          <select v-model="ar.id_volumen" class="form-select form-select-sm" aria-label=".form-control-sm example" id= "selectVolumen">
+            <option v-for="m of volumenesMateriales"
+              :value="m.id"
+            >{{ m.categoria }}</option>
+          </select>
+        </div>
+        <div class="form-group mb-3">
+          <label for="formFile">Cargar imagen de los materiales <small>(opcional)</small></label>
+          <input-file v-model="ar.foto" type="img" accept="image/*"></input-file>
+        </div>
+        
+        <div class="d-grid gap-2">
+          <button :disabled="!verificado || posting" class="btn btn-primary btn-block w-100 py-2 text-uppercase">
+            <template v-if="!posting">Enviar</template>
+            <div v-else class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </button>
+          <button @click.prevent="$router.go(-1)" class="btn btn-link btn-block w-100 py-2 text-uppercase">Cancelar</button>
+        </div>
+        <p v-if="mensajeError != ''" class="alert alert-danger mt-4">{{ mensajeError }}</p>
+      </form>
     </div>
-    <p v-if="mensajeError != ''" class="alert alert-danger mt-4">{{ mensajeError }}</p>
-  </form>
+  
+  <div class="col-md d-none d-md-block"></div>
+</div>
 
   <div id="modal-respuesta" class="modal modal-fullscreen-sm-down" role="dialog">
     <div class="modal-dialog modal-dialog-centered" role="document">
