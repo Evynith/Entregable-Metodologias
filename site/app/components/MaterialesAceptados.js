e1155d33afc6b8fa6579b2a694c4517e7248d81f
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2021-06-16 18:24:35 -0300

    #CR-164 responsive del área materiales aceptados

--- site/app/components/MaterialesAceptados.js
+++ site/app/components/MaterialesAceptados.js
@@ -8,37 +8,45 @@ const MaterialesAceptadosTemplate = `
   </div>
 </div>
 <section v-else>
-  <h1 class="fs-1 text-center mb-4">Materiales aceptados</h1>
+  <h1 class="fs-1 mb-4">Materiales aceptados</h1>
 
-  <ul class="nav d-flex justify-content-between mb-3" id="pills-tab" role="tablist">
-    <li v-for="(m, i) of materiales" role="presentation" class="my-1" style="width: 32%;">
-      <button type="button" role="tab" data-bs-toggle="pill"
-        :id="getPillsId(i)"
-        :class="['card', 'px-0', { 'active': i == 0} ]"
-        :data-bs-target="getTabTarget(i)" 
-        :aria-controls="getTabId(i)"
-        @click="scrollToTabContent"
-      >
-        <img :src="m.imagen" class="card-img-top">
-        <span v-html="m.nombre" class="w-100 text-center py-1 small"></span>
-      </button>
-    </li>
-  </ul>
+<div class="row">
   
-  <div class="tab-content" id="pills-tabContent">
-    <div v-for="(m, i) of materiales" 
-      :class="['card', 'tab-pane', 'fade', { 'show': i == 0, 'active': i == 0 }]" 
-      :id="getTabId(i)" 
-      :aria-labelledby="getPillsId(i)" role="tabpanel"
-    >
-      <h5 class="card-header" v-html="m.nombre"></h5>
-      <div class="card-body">
-        <!-- <h5 class="card-title">Special title treatment</h5> -->
-        <p class="card-text" v-html="m.descripcion"></p>
-        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  <div class="col-md-4">
+    <ul class="nav row row-cols-3 row-cols-md-2 me-2 ms-2" id="pills-tab" role="tablist">
+      <li v-for="(m, i) of materiales" role="presentation" class="my-1 ps-1 pe-1">
+        <button type="button" role="tab" data-bs-toggle="pill"
+          :id="getPillsId(i)"
+          :class="['card', 'col', 'px-0', { 'active': i == 0} ]"
+          :data-bs-target="getTabTarget(i)" 
+          :aria-controls="getTabId(i)"
+          @click="scrollToTabContent"
+        >
+          <img :src="m.imagen" class="card-img-top">
+          <span v-html="m.nombre" class="w-100 text-center py-1 small"></span>
+        </button>
+      </li>
+    </ul>
+  </div>
+  
+  <div class="col-md-8">
+    <div class="tab-content" id="pills-tabContent">
+      <div v-for="(m, i) of materiales" 
+        :class="['card', 'tab-pane', 'fade', { 'show': i == 0, 'active': i == 0 }]" 
+        :id="getTabId(i)" 
+        :aria-labelledby="getPillsId(i)" role="tabpanel"
+      >
+        <h5 class="card-header" v-html="m.nombre"></h5>
+        <div class="card-body">
+          <!-- <h5 class="card-title">Special title treatment</h5> -->
+          <p class="card-text" v-html="m.descripcion"></p>
+          <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        </div>
       </div>
     </div>
   </div>
+  
+</div>
 
 </section>
 `
