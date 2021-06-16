f665346b680084b8defd4179dbc263599f6a51cb
Evelyn <cvega@alumnos.exa.unicen.edu.ar>
2021-06-16 17:07:50 -0300

    #CR-164 responsive del área Home

--- site/app/components/Home.js
+++ site/app/components/Home.js
@@ -29,29 +29,36 @@ export default {
     </div>
     
     <h1 class="text-center m-4">¡Hacé tu parte!</h1>
+    
+    <div class="row">
+        <div class="col-md d-none d-md-block"></div>
 
-    <div class="mb-2">
-        <div class="d-flex flex-row">
-            <p class="fibron fs-1 m-2">1.</p>
-            <p><b>Informate acerca de que materiales aceptamos y como prepararlos.</b></p>
-        </div>
-
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <router-link to="/materiales-aceptados" class="btn btn-primary">Ver materiales aceptados</router-link>
-        </div>
+        <div class="col-md">
+            <div class="mb-2">
+                <div class="d-flex flex-row">
+                    <p class="fibron fs-1 m-2">1.</p>
+                    <p><b>Informate acerca de que materiales aceptamos y como prepararlos.</b></p>
+                </div>
         
-    </div>
-    <div class="mb-2">
-        <div class="d-flex flex-row">
-            <p class="fibron fs-1 m-2">2.</p>
-            <p><b>Generá avisos de retiro de materiales para que nuestros recicladores pasen a buscarlos.</b></p>
-        </div>
-
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <router-link to="/ofrecer-materiales" class="btn btn-primary">Generar aviso de retiro</router-link>
-        </div>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <router-link to="/materiales-aceptados" class="btn btn-primary">Ver materiales aceptados</router-link>
+                </div>
+                
+            </div>
+            <div class="mb-2">
+                <div class="d-flex flex-row">
+                    <p class="fibron fs-1 m-2">2.</p>
+                    <p><b>Generá avisos de retiro de materiales para que nuestros recicladores pasen a buscarlos.</b></p>
+                </div>
         
-    </div>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <router-link to="/ofrecer-materiales" class="btn btn-primary">Generar aviso de retiro</router-link>
+                </div>
+                
+            </div>
+        </div>
 
+        <div class="col-md d-none d-md-block"></div>
+    </div>
     `
 }
