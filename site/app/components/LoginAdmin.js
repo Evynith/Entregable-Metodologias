22bbe6cb1b09922bde5f979f6a13449a69157784
Evelyn Vega <cvega@alumnos.exa.unicen.edu.ar>
2021-07-01 17:57:26 -0300

    CR-179 se implementó interfaz sin asociacion a API de login de secretaria (admin)

--- site/app/components/LoginAdmin.js
+++ site/app/components/LoginAdmin.js
@@ -12,19 +12,20 @@ const LoginAdminTemplate =`
             <div class="">
                 <h1 class="fs-1 text-center"> ¡Bienvenide!</h1>
                 <p>Ingrese su usuario y contraseña para acceder al sistema</p>
-                <form action="" id= "form-la">
+                <form @submit.prevent="post">
                     <div class="form-group mb-2">
                         <label>Usuario</label>
-                        <input v-model.lazy="la.usuario" type="text" class="form-control form-control-sm">
+                        <input v-model="datosLogin.usuario" type="text" class="form-control form-control-sm">
                     </div>
                     <div class="form-group mb-2">
                         <label>Contraseña</label>
-                        <input v-model.lazy="la.contrasenia" type="password" class="form-control form-control-sm">
+                        <input v-model="datosLogin.contrasenia" type="password" class="form-control form-control-sm">
                     </div>
         
                     <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="button">Ingresar</button>
+                        <button class="btn btn-primary">Ingresar</button>
                     </div>
+                    <p v-if="mensajeError != ''" class="alert alert-danger mt-4">{{ mensajeError }}</p>
                 </form>
             </div>
         </div>
@@ -34,10 +35,26 @@ const LoginAdminTemplate =`
     `
 export default {
     data() {
-        return {
-            la: {
-                nombre: '',
-                contrasenia: ''
+            return {
+                datosLogin: {
+                    usuario: "",
+                    contrasenia: ""
+                },
+                mensajeError: ''
+        }
+    },
+    computed: {
+        verificado() {
+            return this.datosLogin.contrasenia != "" && this.datosLogin.usuario != ""
+        }
+    },
+    methods: {
+        post() {
+            if(this.verificado){
+                console.log("posteando", this.datosLogin);
+                this.mensajeError = "";
+            } else {
+                this.mensajeError = "Faltan ingresar datos";
             }
         }
     },
