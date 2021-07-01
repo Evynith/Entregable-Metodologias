11a30cc23a540696d63f5e0e2904c7d815e610e6
Evelyn Vega <cvega@alumnos.exa.unicen.edu.ar>
2021-07-01 18:35:00 -0300

    CR-192 se implementó interfaz sin asociacion a API de registro de secretaria (admin)

--- site/app/components/RegistroAdmin.js
+++ site/app/components/RegistroAdmin.js
@@ -12,32 +12,34 @@ const RegistroAdminTemplate =`
             <div class="">
                 <h1 class="fs-1 text-center"> Registrarse</h1>
                 <p> Llene el siguiente formulario con sus datos para poder acceder al sistema y recibir información en su e-mail. </p>
-                <form action="" id= "form-re">
+                <form @submit.prevent="post">
                     <div class="form-group mb-2">
                         <label>Nombre de usuario</label>
-                        <input v-model.lazy="re.usuario" type="text" class="form-control form-control-sm">
+                        <input v-model="datosRegistro.usuario" type="text" class="form-control form-control-sm">
                     </div>
                     <div class="form-group mb-2">
                         <label>Contraseña</label>
-                        <input v-model.lazy="re.contrasenia" type="password" class="form-control form-control-sm">
+                        <input v-model="datosRegistro.contrasenia" type="password" class="form-control form-control-sm">
                     </div>
                     <div class="form-group mb-2">
                         <label>Reingrese su contraseña</label>
-                        <input v-model.lazy="re.contraseniaReingreso" type="password" class="form-control form-control-sm">
+                        <input v-model="contraseniaReingreso" type="password" class="form-control form-control-sm">
                     </div>
                     <div class="form-group mb-2">
                         <label>e-mail</label>
-                        <input v-model.lazy="re.email" type="email" class="form-control form-control-sm">
+                        <input v-model="datosRegistro.email" type="email" class="form-control form-control-sm">
                     </div>
                     <div class="form-group mb-2">
                         <label>Reingrese su e-mail</label>
-                        <input v-model.lazy="re.emailReingreso" type="email" class="form-control form-control-sm">
+                        <input v-model="emailReingreso" type="email" class="form-control form-control-sm">
                     </div>
         
                     <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="button">Registrarse</button>
-                        <button class="btn btn-link" type="button">Cancelar</button>
+                        <button class="btn btn-primary">Registrarse</button>
+                        <button class="btn btn-link">Cancelar</button>
                     </div>
+
+                    <p v-if="mensajeError != ''" class="alert alert-danger mt-4">{{ mensajeError }}</p>
                 </form>
             </div>
         </div>
@@ -48,12 +50,39 @@ const RegistroAdminTemplate =`
 export default {
     data() {
         return {
-            re: {
-                usuario: '',
-                contrasenia: '',
-                contraseniaReingreso: "",
+            datosRegistro: {
+                usuario: "",
+                contrasenia: "",
                 email: "",
-                emailReingreso: ""
+            },
+            mensajeError: "",
+            contraseniaReingreso: "",
+            emailReingreso: ""
+        }
+    },
+    computed: {
+        verificado() {
+            if(this.datosRegistro.usuario && this.datosRegistro.contrasenia && this.datosRegistro.email && this.contraseniaReingreso && this.emailReingreso) {
+                if(this.datosRegistro.contrasenia != this.contraseniaReingreso){
+                    this.mensajeError = "La contraseña no coincide";
+                    return false;
+                } else if  (this.datosRegistro.email != this.emailReingreso) {
+                    this.mensajeError = "El email no coincide";
+                    return false;
+                }else {
+                    this.mensajeError = "";
+                    return true;
+                }
+            } else {
+                this.mensajeError = "Faltan ingresar datos";
+                return false;
+            }
+        }
+    },
+    methods: {
+        post() {
+            if(this.verificado){
+                console.log("posteando", this.datosRegistro);
             }
         }
     },
