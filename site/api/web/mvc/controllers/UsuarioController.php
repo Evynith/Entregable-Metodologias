17baacdc5ef5b16b8d365bc418f826527aaab175
Evelyn Vega <cvega@alumnos.exa.unicen.edu.ar>
2021-07-05 00:31:44 -0300

    CR-189 se corrigió error de contraseña

--- site/api/web/mvc/controllers/UsuarioController.php
+++ site/api/web/mvc/controllers/UsuarioController.php
@@ -29,10 +29,13 @@ class UsuarioController extends ApiController {
                 ]
             );
             if ($respuesta->ok()) {
-                $usuario = $respuesta->get('usuario');
-                $coincidenContrasenias = password_verify($data->contrasenia, $usuario->contrasenia);
+                $usuario = $respuesta->get("usuario");
+
+                // var_dump($usuario[0]->contrasenia);
+
+                $coincidenContrasenias = password_verify($data->contrasenia, $usuario[0]->contrasenia);
                 if($coincidenContrasenias) {
-                    Auth::login($usuario);
+                    // Auth::login($usuario);
                 } else {
                     $respuesta->setError(new Exception("La contraseña no coincide", 400));
                 }
@@ -50,9 +53,11 @@ class UsuarioController extends ApiController {
         $respuesta = new Respuesta; 
         $data = $this->getData();
 
-        // console.log($data);
+        // var_dump($data);
 
         if (!empty($data->usuario) && !empty($data->contrasenia) && !empty($data->email)){
+
+            var_dump(password_hash('admin', PASSWORD_DEFAULT));
 
             $data->contrasenia = password_hash($data->contrasenia, PASSWORD_DEFAULT);
             $respuesta = $this->modelUsuario->post($data,
